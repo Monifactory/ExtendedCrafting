f8c254bf0b3be3d70aa2a5d412350548b793cabd
WithersChat <witherschat@gmail.com>
2024-09-13 14:51:44 +0200

    More comments + Ultimate Singularity in Tier 5 table
    - UltimateSingularityRecipe is... a recipe of all time, for sure.

--- src/main/java/com/blakebr0/extendedcrafting/api/crafting/ITableRecipe.java
+++ src/main/java/com/blakebr0/extendedcrafting/api/crafting/ITableRecipe.java
@@ -7,6 +7,16 @@ import net.minecraft.world.item.crafting.Recipe;
  * Used to represent an Extended Crafting Table recipe for the recipe type
  */
 public interface ITableRecipe extends Recipe<Container> {
+
+    /**
+     * Returns whether the recipe is tier-locked
+     * @return Whether the recipe is tier-locked
+     */
     int getTier();
+
+    /**
+     * Returns whether the recipe is tier-locked
+     * @return Whether the recipe is tier-locked
+     */
     boolean hasRequiredTier();
 }
--- src/main/java/com/blakebr0/extendedcrafting/crafting/recipe/UltimateSingularityRecipe.java
+++ src/main/java/com/blakebr0/extendedcrafting/crafting/recipe/UltimateSingularityRecipe.java
@@ -13,13 +13,32 @@ import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraftforge.items.IItemHandler;
 
+/**
+ * Special shapeless recipe type for the Ultimate Singularity
+ * with extra utility methods.
+ */
 public class UltimateSingularityRecipe extends ShapelessTableRecipe {
     private static boolean ingredientsLoaded = false;
 
+    /**
+     * Creates an instance of the UltimateSingularityRecipe.
+     * Something noteworthy is that the inputs list is a currently empty
+     * yet MUTABLE list, which allows UltimateSingularityRecipe::getIngredients
+     * to define the inputs.
+     * The recipe is always locked to table tier 5.
+     * @param recipeId Recipe ID (resource location)
+     * @param output Recipe output
+     */
     public UltimateSingularityRecipe(ResourceLocation recipeId, ItemStack output) {
-        super(recipeId, NonNullList.create(), output, 4);
+        super(recipeId, NonNullList.create(), output, 5);
     }
 
+    /**
+     * If ingredients are loaded, just returns a list of input.
+     * If ingredients aren't loaded yet, dynamically loads a list of singularities
+     * as inputs.
+     * @return List of recipe ingredients
+     */
     @Override
     public NonNullList<Ingredient> getIngredients() {
         if (!ingredientsLoaded) {
@@ -28,7 +47,7 @@ public class UltimateSingularityRecipe extends ShapelessTableRecipe {
             SingularityRegistry.getInstance().getSingularities()
                     .stream()
                     .filter(singularity -> singularity.isInUltimateSingularity() && singularity.getIngredient() != Ingredient.EMPTY)
-                    .limit(81)
+                    .limit(121)
                     .map(SingularityUtils::getItemForSingularity)
                     .map(Ingredient::of)
                     .forEach(super.getIngredients()::add);
@@ -39,6 +58,13 @@ public class UltimateSingularityRecipe extends ShapelessTableRecipe {
         return super.getIngredients();
     }
 
+    /**
+     * Checks whether the contents of the table matches the grid,
+     * but only returns true if the list of ingredients,
+     * which is dynamically generated, is not empty.
+     * @param inventory The table's inventory
+     * @return Whether the contents of the table match the recipe
+     */
     @Override
     public boolean matches(IItemHandler inventory) {
         // ensure ingredients list is initialized
@@ -53,21 +79,51 @@ public class UltimateSingularityRecipe extends ShapelessTableRecipe {
         return ModRecipeSerializers.ULTIMATE_SINGULARITY.get();
     }
 
+    /**
+     * Invalidates the recipe's ingredients, prompting
+     * ::getIngredients to load them again.
+     */
     public static void invalidate() {
         ingredientsLoaded = false;
     }
 
+    /**
+     * Recipe serializer. Defines:
+     * - Serializer::fromJson
+     * - Serializer::fromNetwork
+     * - Serializer::toNetwork
+     */
     public static class Serializer implements RecipeSerializer<UltimateSingularityRecipe> {
+
+        /**
+         * Creates a UltimateSingularityRecipe with the given recipeId.
+         * Effectively ignores the contents of the JSON object.
+         * @param recipeId Recipe ID
+         * @param json JSON object that doesn't contain the recipe's data since it's auto generated
+         * @return Recipe for the Ultimate Singularity
+         */
         @Override
         public UltimateSingularityRecipe fromJson(ResourceLocation recipeId, JsonObject json) {
             return new UltimateSingularityRecipe(recipeId, new ItemStack(ModItems.ULTIMATE_SINGULARITY.get()));
         }
 
+        /**
+         * Just recreates the recipe since it's always the same.
+         * @param recipeId Recipe ID
+         * @param buffer Useless, please ignore.
+         * @return Recipe for the Ultimate Singularity
+         */
         @Override
         public UltimateSingularityRecipe fromNetwork(ResourceLocation recipeId, FriendlyByteBuf buffer) {
             return new UltimateSingularityRecipe(recipeId, new ItemStack(ModItems.ULTIMATE_SINGULARITY.get()));
         }
 
+        /**
+         * Doesn't encode anything. The recipe is recreated in ::fromNetwork anyway.
+         * *visible disbelief*
+         * @param buffer Useless, please ignore
+         * @param recipe Useless, please ignore
+         */
         @Override
         public void toNetwork(FriendlyByteBuf buffer, UltimateSingularityRecipe recipe) { }
     }
